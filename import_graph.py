851967e056de3e56cf1874e9147b8f608fbb3f95
QLutz <qlutz@enst.fr>
2018-03-11 21:23:31 +0100

    Add files via upload

--- import_graph.py
+++ import_graph.py
@@ -1,39 +1,40 @@
 import networkx as nx
+import os
 
-def import_graph(directory,dataset):
+def import_graph(dataset):
     """Import a graph stored in text files into a NetworkX graph."""
     try:
-        f = open(directory + dataset + "/type.txt", "r")
+        f = open( os.path.join(dataset , "type.txt"), "r")
         type_of_graph = f.readline()[0:-1]
         f.close()
         if type_of_graph == "DW":
             G = nx.read_weighted_edgelist(
-                directory + dataset + "/edge.txt",
+                os.path.join(dataset , "edge.txt"),
                 nodetype=int,
                 create_using=nx.DiGraph())
         elif type_of_graph == "UW":
             G = nx.read_weighted_edgelist(
-                directory + dataset + "/edge.txt",
+                os.path.join(dataset , "edge.txt"),
                 nodetype=int)
         elif type_of_graph == "DU":
             G = nx.read_edgelist(
-                directory + dataset + "/edge.txt",
+                os.path.join(dataset , "edge.txt"),
                 nodetype=int,
                 create_using=nx.DiGraph())
         else:
             G = nx.read_edgelist(
-                directory + dataset + "/edge.txt",
+                os.path.join(dataset , "edge.txt"),
                 nodetype=int)
         G.name = dataset
     except:
         G = nx.Graph(name = "Empty graph")
     return G
 
-def import_position(directory,dataset):
+def import_position(dataset):
     """Import the position layout associated with a graph."""
     pos = {}
     try:
-        f = open(directory + dataset + "/position.txt", "r")
+        f = open(os.path.join(dataset , "position.txt"), "r")
         u = 0
         for line in f:
             s = line.split()
@@ -44,11 +45,11 @@ def import_position(directory,dataset):
         pass
     return pos
 
-def import_label(directory,dataset):
+def import_label(dataset):
     """Import the labels associated with a graph."""
     label = []
     try:
-        f = open(directory + dataset + "/label.txt", "r")
+        f = open(os.path.join(dataset , "label.txt"), "r")
         for line in f:
             label.append(line[0:-1])
         f.close()
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
+import os
 
 from algo import edge_cluster
 from import_graph import *
 from display import edge_cluster_graph,dict2list
 
-directory = r'C:\Users\Quentin\Documents\2A\RES208-20170630T155843Z-001\RES208\dataset\\'
-dataset = sys.argv[1]
+script_dir = os.path.dirname(os.path.realpath('__file__'))
+dataset_name = sys.argv[1]
+full_path = os.path.join(script_dir,"dataset",dataset_name)
 
 #Import the graph and associated info
-G = import_graph(directory,dataset).to_undirected()
-pos = import_position(directory,dataset)
-label = import_label(directory,dataset)
+G = import_graph(full_path).to_undirected()
+pos = import_position(full_path)
+label = import_label(full_path)
 node_from_label = {l:i for i,l in enumerate(label)}
 #Clustering, this may take a while
 com = edge_cluster(G)
